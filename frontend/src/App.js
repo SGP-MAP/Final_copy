e8941ad8e3687171a99dfcb81006c6228b9a470a
manthan0112 <shahmanthan0112@gmail.com>
2022-05-07 22:30:20 +0530

    copose file updated

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -36,7 +36,7 @@ export default class Paths extends Component {
     }
 
     componentDidMount() {
-        sessionStorage.setItem("ip_add", "34.229.151.247");
+        sessionStorage.setItem("ip_add", "spring");
     }
 
     render(){
